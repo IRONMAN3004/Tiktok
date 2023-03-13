bb7055b3b902cc3a2ec943c94ea168bb53d6e2cd
ik53phamvannam@gmail.com <ik53phamvannam@gmail.com>
2023-03-13 21:36:25 +0700

    Cài đặt babel-plugin-module-resolver

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+/* config-overrides.js */
+
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+  const { override, useBabelRc } = require("customize-cra");
+
+  module.exports = override(
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useBabelRc()
+    );
+  return config;
+};
