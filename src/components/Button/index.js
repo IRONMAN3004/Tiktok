98c922c3e9f80024ba559b2581eb3b1d38fd446b
ik53phamvannam@gmail.com <ik53phamvannam@gmail.com>
2023-03-17 12:10:45 +0700

    Xây dựng UI phần Header #3

---
+++ src/components/Button/index.js
@@ -0,0 +1,75 @@
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import { Link } from 'react-router-dom';
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    disable = false,
+    rounded = false,
+    small = false,
+    large = false, //ko truyền trong component cha thì mặc định nó lấy giá trị dèault là false
+    children,
+    onClick,
+    className,
+    leftIcon,
+    rightIcon,
+    ...passProp // trường hợp những props xuất hiện đột xuất
+}) {
+    const cx = classNames.bind(styles);
+    // let Comp = 'p';
+    let Comp = 'button';
+
+    const classes = cx('wrapper', {
+        primary,
+        // primary: primary,
+        [className]: className,
+        outline,
+        small,
+        large,
+        text,
+        disable,
+        rounded,
+    });
+
+    const _props = {
+        onClick,
+        ...passProp,
+    };
+
+    //Remove event listerner when button is disable
+    if (disable) {
+        // delete _props.onClick;
+        //  console.log(Object.keys(_props)); // Object.keys(_props) trả về 1 mảng gồm các key của object oput: ['onClick', 'onMouseUp']
+
+        Object.keys(_props).forEach((key) => {
+            if (key.startsWith('on') && typeof _props[key] === 'function') {
+                // console.log(_props[key]); //tức lấy value của key(biến) là key
+                // console.log(key);
+                delete _props[key];
+            }
+        });
+    }
+
+    if (to) {
+        _props.to = to; //đây là cách gán thêm thuộc tính mới cho đối tượng sau khi đã khai báo
+        Comp = Link;
+    } else if (href) {
+        _props.href = href;
+        Comp = 'a';
+    }
+    // console.log(leftIcon);
+
+    return (
+        <Comp className={classes} {..._props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -2,8 +2,9 @@ import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
 import images from '~/asset/images';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faMagnifyingGlass, faSignIn, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import AccountItem from '~/components/AccountItem';
+import Button from '~/components/Button';
 
 import Tippy from '@tippyjs/react/headless';
 // import 'tippy.js/dist/tippy.css';
@@ -60,7 +61,14 @@ function Header() {
                     </div>
                 </Tippy>
 
-                <div className={cx('action')}></div>
+                <div className={cx('action')}>
+                    {/* <Button primary href="/"> */}
+                    <Button text>Upload</Button>
+                    {/* Trường hợp tự custom ở 1 file khác chứ ko phải ở component custom thì thêm classname vào className={cx('custom-login')}} */}
+                    <Button primary rightIcon={<FontAwesomeIcon icon={faSignIn} />}>
+                        Login
+                    </Button>
+                </div>
             </div>
         </header>
     );
